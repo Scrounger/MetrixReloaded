01604891156bd142a28847eaa70cf022d33fb731
Scrounger <scrounger@gmx.net>
2019-03-18 17:53:43 +0100

    MetrixReloaded/MetrixReloadedSetup: version added, hard coded screen file removed

--- lib/Plugins/Extensions/MetrixReloaded/MetrixReloadedSetup.py
+++ lib/Plugins/Extensions/MetrixReloaded/MetrixReloadedSetup.py
@@ -6,40 +6,24 @@ from Components.ConfigList import ConfigListScreen
 from Screens.MessageBox import MessageBox
 from Screens.FileDirBrowser import FileDirBrowser
 
+
 # Configuration
 from Components.config import config, getConfigListEntry
 
 # GUI (Components)
 from Components.ActionMap import ActionMap
 from Components.Sources.StaticText import StaticText
+from Components.Label import Label
 
 import os
 
 # MyLog
-from Tools.MetrixReloadedHelper import initializeLog
+from Tools.MetrixReloadedHelper import initializeLog, getVersion
 
 #from Tools import Notifications
 
 
 class MetrixReloadedSetup(Screen, ConfigListScreen):
-    """Configuration of MetrixReloaded"""
-
-    skin = """<screen name="EPGRefreshConfiguration" position="center,center" size="600,430">
-		<ePixmap position="0,5" size="140,40" pixmap="skin_default/buttons/red.png" transparent="1" alphatest="on" />
-		<ePixmap position="140,5" size="140,40" pixmap="skin_default/buttons/green.png" transparent="1" alphatest="on" />
-		<ePixmap position="280,5" size="140,40" pixmap="skin_default/buttons/yellow.png" transparent="1" alphatest="on" />
-		<ePixmap position="420,5" size="140,40" pixmap="skin_default/buttons/blue.png" transparent="1" alphatest="on" />
-		<ePixmap position="562,15" size="35,25" pixmap="skin_default/buttons/key_info.png" alphatest="on" />
-
-		<widget source="key_red" render="Label" position="0,5" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget source="key_green" render="Label" position="140,5" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget source="key_yellow" render="Label" position="280,5" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-		<widget source="key_blue" render="Label" position="420,5" zPosition="1" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" foregroundColor="white" shadowColor="black" shadowOffset="-1,-1" />
-
-		<widget name="config" position="5,50" size="590,275" scrollbarMode="showOnDemand" />
-		<ePixmap pixmap="skin_default/div-h.png" position="0,335" zPosition="1" size="565,2" />
-		<widget source="help" render="Label" position="5,345" size="590,83" font="Regular;21" />
-	</screen>"""
 
     def __init__(self, session):
         Screen.__init__(self, session)
@@ -82,6 +66,7 @@ class MetrixReloadedSetup(Screen, ConfigListScreen):
             self["key_blue"] = StaticText()
 
             self["help"] = StaticText()
+            self["version"] = StaticText(getVersion())
 
             # Define Actions
             self["actions"] = ActionMap(["SetupActions", "ColorActions", "ChannelSelectEPGActions", "HelpActions"],
