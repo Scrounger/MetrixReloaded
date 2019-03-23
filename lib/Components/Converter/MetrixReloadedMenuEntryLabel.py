b87042e4a90e1a42ea32101d707898cc1652375c
Scrounger <scrounger@gmx.net>
2019-03-23 09:29:47 +0100

    MetrixReloadedMenuEntryLabel: show menu entry -> enable / disable per MetrixReloadedSetup

---
+++ lib/Components/Converter/MetrixReloadedMenuEntryLabel.py
@@ -0,0 +1,35 @@
+from Components.Converter.Converter import Converter
+from Components.Element import cached
+from Components.config import config
+from Poll import Poll
+
+class MetrixReloadedMenuEntryLabel(Poll,Converter,object):
+	def __init__(self, type):
+		Poll.__init__(self)
+		Converter.__init__(self, type)
+		self.poll_interval = 1000
+		self.poll_enabled = True
+		self.type = str(type)
+
+	def selChanged(self):
+		self.downstream_elements.changed((self.CHANGED_ALL, 0))
+
+	@cached
+	def getText(self):
+		cur = self.source.current
+		if cur and len(cur) > 2:
+			selectedMenu = cur[2]
+		
+		if(config.plugins.MetrixReloaded.showScreenNames.value == True):
+			return selectedMenu
+		else:
+			return ''
+	
+	text = property(getText)
+
+	def changed(self, what):
+		if what[0] == self.CHANGED_DEFAULT:
+			self.source.onSelectionChanged.append(self.selChanged)		
+		Converter.changed(self, what)
+
+		
