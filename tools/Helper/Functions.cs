f5e7639b388106ab1ec72c1d8c95c93c34e8bb9c
Scrounger <scrounger@gmx.net>
2019-04-03 11:05:59 +0200

    PiconManagerScreen.xml edited

---
+++ tools/Helper/Functions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Helper
+{
+    public class Functions
+    {
+        public static void CopyAll(string SourcePath, string DestinationPath, bool createDirs, String message = null)
+        {
+            Console.WriteLine();
+            if (message == null)
+            {
+                Console.WriteLine(String.Format("copy files: '{0}' to '{1}'", SourcePath, DestinationPath));
+            }
+            else
+            {
+                Console.WriteLine(message);
+            }
+            Console.WriteLine("---------------------------------------------------------------------------------------------------");
+            //Now Create all of the directories
+            int i = 1;
+
+            string[] dirList = Directory.GetDirectories(SourcePath, "*", SearchOption.AllDirectories);
+            if (createDirs)
+            {
+                foreach (string dirPath in dirList)
+                {
+                    Directory.CreateDirectory(Path.Combine(DestinationPath, dirPath.Remove(0, SourcePath.Length)));
+
+                    ProgressBar.Draw("create directories...", i, dirList.Length);
+                    i++;
+                }
+                Console.WriteLine();
+            }
+            i = 1;
+            dirList = Directory.GetFiles(SourcePath, "*.*", SearchOption.AllDirectories);
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in dirList)
+            {
+                File.Copy(newPath, Path.Combine(DestinationPath, newPath.Remove(0, SourcePath.Length)), true);
+                ProgressBar.Draw("copy files...", i, dirList.Length);
+                i++;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
