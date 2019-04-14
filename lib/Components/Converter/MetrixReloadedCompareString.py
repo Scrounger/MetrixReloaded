7d12a08f95291bfd52f3746cc125945dd29c6850
Scrounger <scrounger@gmx.net>
2019-04-14 11:07:11 +0200

    MetrixReloadedCompareString: NotEmpty function added

--- lib/Components/Converter/MetrixReloadedCompareString.py
+++ lib/Components/Converter/MetrixReloadedCompareString.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from Converter import Converter
 from Components.Element import cached
+import re
 
 class MetrixReloadedCompareString(Converter, object):
 	def __init__(self, arg):
@@ -14,6 +16,16 @@ class MetrixReloadedCompareString(Converter, object):
 
 	@cached
 	def getText(self):
+		#Prüfen ob Wert nicht null ist -> indivduelle Rückgabe
+		parser = re.match(r'^[[]NotEmpty[(](.*)[)][]]', self.value)
+		if(parser != None):
+			if(self.source.text.lower() != None and self.source.text.lower() != ''):
+				return parser.group(1)
+
+		if(self.value == "[NotEmpty]"):
+			if(self.source.text.lower() != None and self.source.text.lower() != ''):
+				return self.source.text.lower()
+
 		if(self.source.text.lower() == self.value.lower()):
 			return self.source.text
 		
